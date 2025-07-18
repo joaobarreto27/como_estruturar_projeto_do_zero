104bde04a00b616d9c23b6a79211c2f1b19f13f1
joaobarreto27 <joao.vito1951@gmail.com>
2025-07-17 21:18:21 -0300

    feat: Adicionando configuração inicial do mkdocs.

---
+++ tests/test_pipeline.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from src.infrastructure.data.pipeline import transform
+
+df_1 = pd.DataFrame({"col1": [1, 2], "col2": [3, 4]})
+df_2 = pd.DataFrame({"col1": [5, 6], "col2": [7, 8]})
+
+
+def testar_a_concatenacao_da_lista_de_dataframe():
+    """use o arrange act e assert para testar a função contact_data_frames"""
+
+    # arrange
+    data_frame_list = [df_1, df_2]
+    data_frame = pd.concat(data_frame_list, ignore_index=True)
+
+    # act
+    df = transform.contact_data_frames(data_frame_list)
+
+    # assert
+    assert df.shape == (4, 2)
+    assert data_frame.equals(df)
+    assert df.shape != (5, 2)
